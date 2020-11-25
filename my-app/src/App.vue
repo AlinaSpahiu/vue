c1332b5b2a4838e8e5f738df1563633c9ce0ef1b
AlinaSpahiu <alina.s.abbaz@gmail.com>
2020-11-25 23:41:23 +0100

    Update App.vue

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,46 +1,46 @@
-
 <template>
-<div id="app">
-      <h3> Hello there! </h3>
-      <!-- <Test /> -->
-      <h2> {{name}}</h2>
+  <div id="app">
+    <Test />
   </div>
 </template>
 
-
-
 <script>
-// import Test from './components/Test'
-const data = {
-  "name": "Alina Spahiu",
-  "about": "Full stack Developer more focused in MERN and MEVN stacks",
-  "image" : "https://media-exp1.licdn.com/dms/image/C4D03AQG3KDiMRNm3Rw/profile-displayphoto-shrink_800_800/0/1601975574996?e=1611792000&v=beta&t=5a8cM4fmp7G7Y7P4HCINfdk01B_XigTLkfP6ueTMl1g",
-   "age" : 27
-}
-export default {
-  name: 'App',
-  components: {
-          // Test,
+import Test from './components/Test'
 
+
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+export default {
+  name: "App",
+  components: {
+    Test,
   },
-  data: data
-}
+  // data() {
+  //   return {
+  //     name: "Alina Spahiu",
+  //     about: "Full stack Developer more focused in MERN and MEVN stacks",
+  //     image:
+  //       "https://media-exp1.licdn.com/dms/image/C4D03AQG3KDiMRNm3Rw/profile-displayphoto-shrink_800_800/0/1601975574996?e=1611792000&v=beta&t=5a8cM4fmp7G7Y7P4HCINfdk01B_XigTLkfP6ueTMl1g",
+  //     age: 27,
+  //   };
+  // },
+};
 </script>
 
-
-
-
 <style>
-*{
+* {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
 }
-body{
+body {
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
 }
-.btn{
+.btn {
   display: inline-block;
   border: none;
   background: #555;
