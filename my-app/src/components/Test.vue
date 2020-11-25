f926a796821562f7e363c47a45154c089d397576
AlinaSpahiu <alina.s.abbaz@gmail.com>
2020-11-25 23:01:56 +0100

    Create Test.vue

---
+++ my-app/src/components/Test.vue
@@ -0,0 +1,29 @@
+<template>
+   <div>
+      <h2> {{this.data.name}}</h2>
+   </div>
+</template>
+
+
+
+<script>
+
+const data = {
+  "name": "Alina Spahiu",
+  "about": "Full stack Developer more focused in MERN and MEVN stacks",
+  "image" : "https://media-exp1.licdn.com/dms/image/C4D03AQG3KDiMRNm3Rw/profile-displayphoto-shrink_800_800/0/1601975574996?e=1611792000&v=beta&t=5a8cM4fmp7G7Y7P4HCINfdk01B_XigTLkfP6ueTMl1g",
+   "age" : 27
+}
+export default {
+    name: "Test",
+    data(){
+      return data
+    }
+}
+</script>
+
+
+
+<style scoped>
+
+</style>
